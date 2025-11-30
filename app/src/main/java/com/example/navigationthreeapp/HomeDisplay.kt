cab24ca2a77c4b4bf9b361ab05aed63a65a5ff25
dao0203 <sato_yuya@tapple.co.jp>
2025-12-01 01:53:24 +0900

    refactor HomeDisplay to simplify navigation handling and remove unused animations

--- app/src/main/java/com/example/navigationthreeapp/HomeDisplay.kt
+++ app/src/main/java/com/example/navigationthreeapp/HomeDisplay.kt
@@ -1,15 +1,9 @@
 package com.example.navigationthreeapp
 
-import androidx.compose.animation.ContentTransform
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
-import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
-import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
 import androidx.compose.runtime.Composable
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
@@ -17,7 +11,7 @@ import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import kotlinx.serialization.Serializable
 
-sealed interface HomeKey: NavKey {
+sealed interface HomeKey : NavKey {
     @Serializable
     data object Screen1 : HomeKey
 
@@ -29,20 +23,10 @@ sealed interface HomeKey: NavKey {
 @Composable
 fun HomeDisplay() {
     val backStack = rememberNavBackStack(HomeKey.Screen1)
-    val listDetailStrategy = rememberListDetailSceneStrategy<NavKey>()
     NavDisplay(
         backStack = backStack,
-        predictivePopTransitionSpec = {
-            ContentTransform(
-                fadeIn(tween(300)),
-                fadeOut(tween(300))
-            )
-        },
-        sceneStrategy = listDetailStrategy,
         entryProvider = entryProvider {
-            entry<HomeKey.Screen1>(
-                metadata = ListDetailSceneStrategy.listPane()
-            ) {
+            entry<HomeKey.Screen1> {
                 LazyColumn {
                     items(10) {
                         Button(
@@ -53,9 +37,7 @@ fun HomeDisplay() {
                     }
                 }
             }
-            entry<HomeKey.Screen2>(
-                metadata = ListDetailSceneStrategy.detailPane()
-            ) {
+            entry<HomeKey.Screen2> {
                 val id = it.id
                 LazyColumn {
                     items(50) {
